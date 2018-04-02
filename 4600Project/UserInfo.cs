568146abe81aacde8448463c1bf3a9a0b567a45a
Chris <ckramer@my.apsu.edu>
2018-04-02 11:11:38 -0500

    Added Classes and files

---
+++ 4600Project/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace _4600Project
+{
+    class UserInfo
+    {
+        [JsonProperty("id")]
+        public long ID { get; set; }
+
+        [JsonProperty("screen_name")]
+        public string ScreenName { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("profile_image_url")]
+        public string ProfileImageUrl { get; set; }
+    }
+}
