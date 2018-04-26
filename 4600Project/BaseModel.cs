ae911f17b0fb70f8d0bccafec938f37b69ad5830
Chris <ckramer@my.apsu.edu>
2018-04-26 10:32:00 -0500

    created models for twitter information.

---
+++ 4600Project/BaseModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _4600Project
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public BaseModel()
+        {
+        }
+
+        protected bool SetProperty<T>(
+            ref T backingStore, T value,
+            [CallerMemberName]string propertyName = "",
+            Action onChanged = null)
+
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ 4600Project/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4600Project
+{
+    public class UserModel
+    {
+        public UserModel(UserEntity user)
+        {
+            UserId = user.Id;
+            UserName = user.Name;
+            ScreenName = user.ScreenName;
+            ProfileImageUrl = user.ProfileImageUrl;
+            TweetRetweetModelList = new List<TweetModel>();
+            TweetModelListWrapper = new TweetModelListWrapper();
+            RetweetModelListWrapper = new TweetModelListWrapper();
+
+        }
+
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+        public string ScreenName { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public List<TweetModel> TweetRetweetModelList { get; set; }
+        public TweetModelListWrapper TweetModelListWrapper { get; set; }
+        public TweetModelListWrapper RetweetModelListWrapper { get; set; }
+       
+       
+      
+    }
+}
